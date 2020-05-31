a78ac02954f584afc53d0391918073501ebc2c14
昱安 陳 <jacky126pro@msn.com>
2020-05-31 17:14:00 +0800

    加入專案檔案。

---
+++ CorePhotoAlbum/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CorePhotoAlbum.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
